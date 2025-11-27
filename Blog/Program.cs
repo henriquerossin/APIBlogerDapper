0d491f946ed290b51056d739b41ec1aff8669a16
Henrique <pro.henriquerossin@gmail.com>
2025-11-27 09:14:38 -0300

    Add project files.

---
+++ Blog/Program.cs
@@ -0,0 +1,31 @@
+using Blog.API.Data;
+using Blog.API.Repositories;
+using Blog.API.Repositories.Interfaces;
+using Blog.API.Services;
+using Blog.API.Services.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddTransient<ConnectionDB>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+builder.Services.AddScoped<ITagRepository, TagRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IRoleService, RoleService>();
+builder.Services.AddScoped<ITagService, TagService>();
+builder.Services.AddScoped<IUserService, UserService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
